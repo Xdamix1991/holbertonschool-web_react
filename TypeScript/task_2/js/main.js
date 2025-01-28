cb1218beed4fba86b886e25b902986d371c5b40a
Xdamix1991 <bourichalamine1@gmail.com>
2025-01-28 15:16:54 +0100

    task6 and 7

--- TypeScript/task_2/js/main.js
+++ TypeScript/task_2/js/main.js
@@ -2,13 +2,13 @@ var Director = /** @class */ (function () {
     function Director() {
     }
     Director.prototype.workFromHome = function () {
-        return ('Working from home');
+        return 'Working from home';
     };
     Director.prototype.getCoffeeBreak = function () {
-        return ('Getting a coffee break');
+        return 'Getting a coffee break';
     };
     Director.prototype.workDirectorTasks = function () {
-        return ('Getting to director tasks');
+        return 'Getting to director tasks';
     };
     return Director;
 }());
@@ -16,13 +16,13 @@ var Teacher = /** @class */ (function () {
     function Teacher() {
     }
     Teacher.prototype.workFromHome = function () {
-        return ('Cannot work from home');
+        return 'Cannot work from home';
     };
     Teacher.prototype.getCoffeeBreak = function () {
-        return ('Cannot have a break');
+        return 'Cannot have a break';
     };
     Teacher.prototype.workTeacherTasks = function () {
-        return ('Getting to work');
+        return 'Getting to work';
     };
     return Teacher;
 }());
@@ -32,7 +32,32 @@ function createEmployee(salary) {
     }
     return new Director;
 }
+function isDirector(employee) {
+    return employee instanceof Director;
+}
+function executeWork(employee) {
+    if (employee instanceof Director) {
+        console.log(employee.workDirectorTasks());
+    }
+    if (employee instanceof Teacher) {
+        console.log(employee.workTeacherTasks());
+    }
+}
+function teachClass(todayClass) {
+    if (todayClass === "Math") {
+        console.log('Teaching Math');
+        return 'Teaching Math';
+    }
+    if (todayClass === "History") {
+        console.log('Teaching History');
+        return 'Teaching History';
+    }
+}
 var employee_1 = createEmployee(400);
 var employee_2 = createEmployee(600);
 console.log(employee_1);
 console.log(employee_2);
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
+teachClass('Math');
+teachClass('History');
--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -12,37 +12,69 @@ interface TeacherInterface {
 
 class Director implements DirectorInterface {
 	workFromHome(): string {
-		return ('Working from home')
+		return 'Working from home'
 	}
 	getCoffeeBreak(): string {
-		return ('Getting a coffee break')
+		return 'Getting a coffee break'
 	}
 	workDirectorTasks(): string {
-		return ('Getting to director tasks')
+		return 'Getting to director tasks'
 	}
 }
 
 class Teacher implements TeacherInterface {
 	workFromHome(): string {
-		return ('Cannot work from home')
+		return 'Cannot work from home'
 	}
 	getCoffeeBreak(): string {
-		return ('Cannot have a break')
+		return 'Cannot have a break'
 	}
 	workTeacherTasks(): string {
-		return ('Getting to work')
+		return 'Getting to work'
 	}
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
 	if (typeof salary === "number" && salary < 500) {
-		return new Teacher
+		return new Teacher;
 	}
-	return new Director
+	return new Director;
+}
+
+function isDirector (employee: unknown): employee is Director {
+	return employee instanceof Director;
+}
+
+function executeWork (employee: unknown) {
+	if (employee instanceof Director) {
+		console.log(employee.workDirectorTasks());
+	}
+	if (employee instanceof Teacher) {
+		console.log(employee.workTeacherTasks());
+	}
+}
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+	if (todayClass === "Math") {
+		console.log('Teaching Math')
+		return 'Teaching Math'
+	}
+	if (todayClass === "History") {
+		console.log('Teaching History')
+		return 'Teaching History'
+	}
 }
 
 const employee_1 = createEmployee(400);
 const employee_2 = createEmployee(600);
 
-console.log(employee_1)
-console.log(employee_2)
+console.log(employee_1);
+console.log(employee_2);
+
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
+
+teachClass('Math');
+teachClass('History');
